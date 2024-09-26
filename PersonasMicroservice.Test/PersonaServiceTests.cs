778a2e167950ae965fd3b93c80e82b9ec1786e1b
Daniel Quintero <dquintero.ing@gmail.com>
2024-09-26 13:33:29 -0500

    Pruebas Unitarias

---
+++ PersonasMicroservice.Test/PersonaServiceTests.cs
@@ -0,0 +1,51 @@
+﻿using AutoMapper;
+using Moq;
+using NUnit.Framework;
+using PersonasMicroservice.Application.Mappings;
+using PersonasMicroservice.Application.Services;
+using PersonasMicroservice.Domain.Entities;
+using PersonasMicroservice.Infrastructure.Repository;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace PersonasMicroservice.Test
+{
+    [TestFixture]
+    public class PersonaServiceTests
+    {
+        private Mock<IPersonaRepository> _mockPersonaRepository;
+        private PersonaService _personaService;
+        private IMapper _mapper;
+
+        [SetUp]
+        public void Setup()
+        {
+            _mockPersonaRepository = new Mock<IPersonaRepository>();
+
+            var config = new MapperConfiguration(cfg => cfg.AddProfile(new PersonaProfile()));
+            _mapper = config.CreateMapper();
+
+            _personaService = new PersonaService(_mockPersonaRepository.Object, _mapper);
+        }
+
+        [Test]
+        public async Task GetAll_ShouldReturnListOfPersonas()
+        {
+            // Arrange (implícito en Setup)
+            _mockPersonaRepository.Setup(repo => repo.GetAll())
+                .ReturnsAsync(new List<Persona>
+                {
+                new Persona { Id = 1, Nombre = "Juan", IdTipoPersona = 1 },
+                new Persona { Id = 2, Nombre = "Ana", IdTipoPersona = 2 }
+                });
+
+            // Act
+            var result = await _personaService.GetAll();
+
+            // Assert
+            Assert.IsNotNull(result);
+            Assert.AreEqual(2, result.Count);
+            Assert.AreEqual("Juan", result[0].Nombre);
+        }
+    }
+}
---
+++ PersonasMicroservice.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("PersonasMicroservice.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("PersonasMicroservice.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("7078e534-637c-4b96-a2cb-ba65204c17aa")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
